16df302ef227994f35c266b753681d823dc2a6a6
Marge Rosen <marge@masterclassu.com>
2013-05-22 11:32:40 -0700

    add more practice files

---
+++ intro_to_ruby/control_flow_script1.rb
@@ -0,0 +1,11 @@
+# control flow ex 1
+print "Integer please: "
+user_num = Integer(gets.chomp)
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end
---
+++ intro_to_ruby/if_script2.rb
@@ -0,0 +1,1 @@
+# control flow ex 2 - IFs
