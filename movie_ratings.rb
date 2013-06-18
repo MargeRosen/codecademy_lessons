211f14e64887636781538f9c5558f4de22d55c21
Marge Rosen <marge@masterclassu.com>
2013-06-18 10:14:51 -0700

    Add ruby CRUD program Movie Ratings

---
+++ movie_ratings.rb
@@ -0,0 +1,65 @@
+=begin  CRUD example
+
+This program will keep track of your personal movie ratings.
+
+Features: add a new movie, update the rating for an existing movie, 
+display the movies and ratings, or delete a movie. If the program doesn't receive one 
+of those four commands, the program will output some kind of error message.
+
+=end
+movies = {
+  Spiderman: 3,
+  Indian_Jones: 4,
+  Bubba_Ho_Tep: 1,
+  Star_Wars: 4
+}
+
+puts "What would you like to do?"
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
+choice = gets.chomp.downcase
+case choice
+when 'add'
+  puts "What movie do you want to add?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "What's the rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been added with a rating of #{rating}."
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+when 'update'
+  puts "What movie do you want to update?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Movie not found!"
+  else
+    puts "What's the new rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been updated with new rating of #{rating}."
+  end
+when 'display'
+  movies.each do |movie, rating|
+    puts "#{movie}: #{rating}"
+  end
+when 'delete'
+  puts "What movie do you want to delete?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Movie not found!"
+  else
+    movies.delete(title.to_sym)
+    puts "#{title} has been removed."
+  end
+else
+  puts "Sorry, I didn't understand you."
+end
+
+
+
